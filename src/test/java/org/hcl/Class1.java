1a2d15f936e11012b8d42dc3a8ba70aa934e6490
Balamurugan80 <balacse93@gmail.com>
2021-11-16 01:40:38 +0530

    Revised

--- src/test/java/org/hcl/Class1.java
+++ src/test/java/org/hcl/Class1.java
@@ -1,5 +1,16 @@
 package org.hcl;
 
 public class Class1 {
+private void method1() {
+	// TODO Auto-generated method stub
 
+}
+private void method2() {
+	// TODO Auto-generated method stub
+
+}
+private void method3() {
+	// TODO Auto-generated method stub
+
+}
 }
